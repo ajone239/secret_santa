a823926b1f9a38a351460dfa9c2600a7765ba5e9
Austin Jones <austinjones@Austin-MBP.local>
2019-11-19 14:41:43 -0500

    Credntial creator fully functional

--- make_creds.py
+++ make_creds.py
@@ -1,5 +1,10 @@
 import sys, os
 
+try:
+   from credentials import *
+except:
+   print("making credentials.py")
+
 def parse_creds(filein, fileout = 'credentials.py'):
    creds = []
    with open(filein) as fin:
@@ -14,12 +19,20 @@ def parse_creds(filein, fileout = 'credentials.py'):
    with open(fileout, "a+") as fout:
       credname = filein.split('.')[0].replace(' ','_')
 
-      fout.write(("{} = {}".format(credname,creds)))
+      if credname not in globals():
+         fout.write(("{} = {}\n".format(credname,creds)))
 
 if __name__ == "__main__":
-   if len(sys.argv) == 3:
-      parse_creds(sys.argv[1],sys.argv[2])
-   elif len(sys.argv) == 2:
+
+   if "SENDER_EMAIL" not in globals():
+      with open('credentials.py', "a+") as fout:
+         fout.write("SENDER_EMAIL = \'{}\'\n".format(input("Sender E-mail: ")))
+
+   if "PASSWORD" not in globals():
+      with open('credentials.py', "a+") as fout:
+         fout.write("PASSWORD = \'{}\'\n".format(input("Password: ")))
+
+   if len(sys.argv) == 2:
       parse_creds(sys.argv[1])
    elif len(sys.argv) == 1:
       for file in [f for f in os.listdir(r"./") if f.endswith(".txt")]:
